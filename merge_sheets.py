efa6a9f2e46f247473c97c1fc7414510a1ce6188
caij3 <whyamiso24.7@gmail.com>
2024-09-01 12:39:02 -0400

    converted merge file to function

--- merge_sheets.py
+++ merge_sheets.py
@@ -1,16 +1,13 @@
 import pandas as pd
 
 # Load the two Excel files into dataframes
-df1 = pd.read_excel('modified_file_1.xlsx')  # Replace with your first Excel file name
-df2 = pd.read_excel('modified_file_2.xlsx')  # Replace with your second Excel file name
-
-# Concatenate the two dataframes
-combined_df = pd.concat([df1, df2], ignore_index=True)
-
-# Optional: Drop duplicate rows if needed
-# combined_df.drop_duplicates(inplace=True)
-
-# Save the combined dataframe back to a new Excel file
-combined_df.to_excel('combined_file.xlsx', index=False)
-
-print("Files combined and saved as 'combined_file.xlsx'.")
+def merge_files(files,output):
+    for i in range(len(files)):
+        df = pd.read_excel(files[i])
+        if i == 0:
+            combined_df = df
+        else:
+            combined_df = pd.concat([combined_df, df], ignore_index=True)
+    combined_df.to_excel(output, index=False)
+    print("Files combined and saved as 'combined_file.xlsx'.")
+    return combined_df
